a2bd296c98b221e2cb27b3ce5120126441c24804
narmanguebraun <narmanguebraun1@gmail.com>
2024-08-05 19:08:35 +0200

    Add header to home page and text gradient page

---
+++ app/text/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import TextGradient from "@/components/TextGradient";
+import Header from "@/components/Header";
+
+export default function Text() {
+  return (
+    <main>
+      <Header />
+      <TextGradient />
+    </main>
+  );
+}
