c6b6b5555104488c94116e9041160b5f4ee7bb63
narmanguebraun <narmanguebraun1@gmail.com>
2024-08-20 11:55:25 +0200

    Move header to layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
 import BackgroundGradientGenerator from "@/components/BackgroundGradientGenerator";
-import Header from "@/components/Header";
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <BackgroundGradientGenerator />
     </main>
   );
--- app/text/page.tsx
+++ app/text/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 
-import Header from "@/components/Header";
 import TextGradientGenerator from "@/components/TextGradientGenerator";
 
 export default function Text() {
   return (
     <main>
-      <Header />
       <TextGradientGenerator />
     </main>
   );
