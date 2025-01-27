1fb35e7366add78e2af7d52cf43e896a75eb44e0
narmanguebraun <narmanguebraun1@gmail.com>
2025-01-27 20:05:21 +0100

    Align center alert

--- components/AlertNotification.tsx
+++ components/AlertNotification.tsx
@@ -9,7 +9,7 @@ export default function AlertNotification({ type, message }: AlertProps) {
   return (
     <div
       role="alert"
-      className={`fixed left-0 top-0 z-20 flex w-full items-center gap-2 bg-black p-4 font-mono text-xs ${alertStyles[type]}`}
+      className={`fixed left-0 top-0 z-20 flex w-full items-center justify-center gap-2 bg-black p-4 font-mono text-xs ${alertStyles[type]}`}
     >
       {alertIcons[type]} {message}
     </div>
